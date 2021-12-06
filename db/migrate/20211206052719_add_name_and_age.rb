ccd2b6668587792f3ddfbdd231b419aa7eecf230
nguyenngocaocuong <nugyenngocaocuong@gmail.com>
2021-12-06 12:31:53 +0700

    Add name age to user

---
+++ db/migrate/20211206052719_add_name_and_age.rb
@@ -0,0 +1,4 @@
+class AddNameAndAge < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_022526) do
+ActiveRecord::Schema.define(version: 2021_12_06_052719) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
